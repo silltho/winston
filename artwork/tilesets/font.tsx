b79a795fa91b8948e9ac7b216749089fea4b294c
silltho <siller.thomas@gmx.net>
2024-01-04 23:02:27 +0100

    improve startpage + add sound effects

---
+++ artwork/tilesets/font.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="font" tilewidth="8" tileheight="8" tilecount="224" columns="16">
+ <image source="../../game/assets/fonts/gbs-mono.png" trans="e0f8cf" width="128" height="112"/>
+</tileset>
