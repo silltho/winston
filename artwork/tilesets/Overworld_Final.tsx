6cc13d2964e7083da9326ef326974a805d2cb8bc
silltho <siller.thomas@gmx.net>
2024-01-06 00:26:43 +0100

    add tilesets

---
+++ artwork/tilesets/Overworld_Final.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Overworld_Final" tilewidth="8" tileheight="8" tilecount="2900" columns="58">
+ <image source="Overworld_Final.png" width="464" height="400"/>
+</tileset>
---
+++ artwork/tilesets/Tiles_FinalLayout.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Tiles_FinalLayout" tilewidth="8" tileheight="8" tilecount="1232" columns="28">
+ <image source="Tiles_FinalLayout.png" width="224" height="352"/>
+</tileset>
